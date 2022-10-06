482ada4262ee1f32901941f738bbcc80997bd4db
Shambu-K <shambukavir958@gmail.com>
2022-10-06 06:25:43 +0530

    modified logo

--- main.py
+++ main.py
@@ -57,9 +57,9 @@ def saveData():
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=50, pady=50, )
+window.config(padx=50, pady=50, bg="white" )
 
-canvas = Canvas(height=200, width=200)
+canvas = Canvas(height=200, width=200, highlightthickness=0)
 logo_img = PhotoImage(file= "logo.png")
 canvas.create_image(100, 100, image= logo_img)
 canvas.grid(row=0, column=1)
